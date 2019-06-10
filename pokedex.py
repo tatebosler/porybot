8c57f0d9d356ca636a3678423b91cd3edc0f889b
Tate Bosler <tatebosler@gmail.com>
2019-06-10 08:34:25 -0500

    fix accuracy float in randomTeam

--- pokedex.py
+++ pokedex.py
@@ -230,7 +230,7 @@ class Pokedex:
 				'type_id': move[9],
 				'power': move[10],
 				'pp': move[11],
-				'accuracy': move[12] / 100.0
+				'accuracy': (move[12] / 100.0 if move[12] is not None else 1.0)
 			} for move in cursor.fetchall()]
 		else:
 			speciesData = cls.get(species)
